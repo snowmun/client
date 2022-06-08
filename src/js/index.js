588b661143021b86932b7a6d89c122f757c7e9b8
snow <smashdojo3@gmail.com>
2022-06-07 22:53:56 -0400

    eliminacion de comentario ajax y  comentario de codigo

--- src/js/index.js
+++ src/js/index.js
@@ -1,29 +1,10 @@
-// $(document).ready(function(){
-//         let valueInput = $("#category").val();
-
-
-//             $.ajax({
-
-//                 type:"GET",
-//                     url:`https://api-bsale-test1.herokuapp.com/api/v0/searchallproduct`,
-//                     dataType: "json",
-//                     success: (data)=>{
-//                             $("#imgProduct").html(`
-//                             <img id="imgProductq" src="${data.url_image}" alt="" class=" img-fluid"/> 
-//                             `); //envia imagen a html
-                    
-//                     }
-//             });
-  
-// });
-// variable html 
-{/* <div id="imgProduct"   class="col-md-6 imagencard  "></div>
-
-<label class="nombreproducto" id="nombreProducto" for="">nombre Producto</label>   */}
+// tomo todo el div que contiene ese id
 const contenedorPadre = document.querySelector("#contenedorPadre");
+// tomo el valor de la categoria
 const category = document.querySelector("#category");
 
 ( async() =>  {
+        // creo un callback y en una constante guardo el objeto que me trae la api 
         const solicitud = await fetch('https://api-bsale-test1.herokuapp.com/api/v0/searchallproduct', {
             method: 'GET', 
             headers:{
@@ -32,45 +13,56 @@ const category = document.querySelector("#category");
         }
 
 )
-const prueba = await solicitud.json()
-const guardarMap = prueba.map(({name,url_image,price}) => {
-       return `<div class="tarjeta">
-                <div class="cuerpo">
+// guardo en un objeto toda la dolicitud del api
+const objetoSolicitud = await solicitud.json()
+// en una constante guardo el mapeo del objeto
+const guardarMap = objetoSolicitud.map(({name,url_image,price}) => {
+        // el punto clave vendria siendo este ya que destructuro el objeto en los campos que estan solicitando
+        // y realizo un return en el cual nos retornara lo que va dentro  del div padre (contenedorPadre)
+       return `<div class="column">
+                <div class="card">
                         <div id="imgProduct" class="col-md-6 imagencard  ">
-                                <img src="${url_image}" />
+                                <img class="imagencard" src="${url_image}" />
                         </div>
 
                         <label class="nombreproducto" id="nombreProducto" for="">${name}</label>
-                </div>
-                <div>
-                        <label class="preciocolor" id="precioProducto" for="">$${price}</label>
-                        <button class="botontienda"><i class="fas fa-cart-plus fa-2x "></i></button>
-                </div>
-        </div>`
-})
-contenedorPadre.innerHTML=guardarMap;
-category.addEventListener('change',({target})=>{
-        const guardarMap2 = prueba.map(({name,url_image,price,category}) => {
-
-                if(category == target.value){
-                        return `<div class="tarjeta">
-                        <div class="cuerpo">
-                                <div id="imgProduct" class="col-md-6 imagencard  ">
-                                        <img src="${url_image}" />
-                                </div>
-        
-                                <label class="nombreproducto" id="nombreProducto" for="">${name}</label>
-                        </div>
                         <div>
                                 <label class="preciocolor" id="precioProducto" for="">$${price}</label>
                                 <button class="botontienda"><i class="fas fa-cart-plus fa-2x "></i></button>
                         </div>
-                        </div>`
+                </div>
+               
+        </div>`
+})
+
+// se repite el mismo paso de arriba pero con una llamado de id 
+contenedorPadre.innerHTML=guardarMap;
+// en este caso del category que capure arriba agrego el evento listener el cual se 
+// activara cuando vea un cambio  tomara toda la info con target 
+category.addEventListener('change',({target})=>{
+        const guardarMap2 = objetoSolicitud.map(({name,url_image,price,category}) => {
+                // luego de desestructurar al igual que arriba solo los datos a trabajar 
+                // realizaremos un if en el cual verificaremos que la categoria sea igual
+                // al targeta.value que es el id clickeado en el html 
+                if(category == target.value){
+                        return `<div class="column">
+                        <div class="card">
+                                <div id="imgProduct" class="col-md-6 imagencard  ">
+                                        <img class="imagencard" src="${url_image}" />
+                                </div>
+        
+                                <label class="nombreproducto" id="nombreProducto" for="">${name}</label>
+                                <div>
+                                        <label class="preciocolor" id="precioProducto" for="">$${price}</label>
+                                        <button class="botontienda"><i class="fas fa-cart-plus fa-2x "></i></button>
+                                </div>
+                        </div>
+                       
+                </div>`
 
                 }
                
          })
-
 
 contenedorPadre.innerHTML=guardarMap2;
 
